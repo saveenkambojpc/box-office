5d872ec9e8812e4d1c786ad56f1d07aff81995e1
Saveen Kamboj <saveenkambojpc@gmail.com>
2022-08-12 16:24:37 +0530

    Displaying API Results and Creating Custom Random Function

---
+++ src/misc/config.js
@@ -0,0 +1,7 @@
+const API_BASE_URL = 'https://api.tvmaze.com';
+
+export async function apiGet(queryString){
+    const response = await fetch(`${API_BASE_URL}${queryString}`);
+    const data = await response.json();
+    return data;
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
+import { apiGet } from "../misc/config";
 
 
 
 
 function Home() {
     const [input, setInput] = useState('');
+    const [result,setResult] = useState(null);
 
     const onChange = (e) => {
         setInput(e.target.value);
     }
 
-    const onSearch = async () => {
-        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${input}`);
-        const data = await response.json();
-        console.log(data);
+    const onSearch =  async() => {
+        const result = await apiGet(`/search/shows?q=${input}`);
+        setResult(result);
+        console.log(result);
     }
 
     const handleSubmit = e => {
@@ -25,12 +27,30 @@ function Home() {
             onSearch();
         }
     }
+    
+    const renderResult = () =>{
+        if(result && result.length == 0){
+            return <div>No Result Found</div>
+        }
 
+        if(result && result.length > 0){
+            return(
+                <div>
+                    {result.map(item=>{
+                         return <div key={item.show.id}>{item.show.name}</div>
+                    })}
+                </div>
+            )
+        }
+        
+        return null;
+    }
 
     return (
         <div>
             <input type="text" value={input} onChange={onChange} onKeyDown={handleKeyDown} />
             <button onClick={handleSubmit}>Submit</button>
+            {renderResult()}
         </div>
     )
 }
