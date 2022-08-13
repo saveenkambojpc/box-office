c8c9d4818f6381f0937e8acf476fa643b5b00c0a
Saveen Kamboj <saveenkambojpc@gmail.com>
2022-08-13 17:08:07 +0530

    Adding usereducer to our show page -->important⚠️

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,23 +1,62 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState , useReducer} from 'react'
 import { useParams } from 'react-router-dom'
 import { apiGet } from '../misc/config';
 
+const initialState = {
+    data:{},
+    isLoading:true,
+    error:null
+}
+
+
+const reducer =(prevState,action)=>{
+    switch(action.type){
+        case 'FETCH_SUCESS':{
+            return {
+                data:action.data,
+                isLoading:false,
+                error:null
+            }
+        }
+        case 'FETCH_FAILED':{
+            return {
+                ...prevState,
+                isLoading: false,
+                error:action.error
+            }
+        }
+
+        default:return prevState;
+    }
+
+}
+
 const Show = () => {
 
-    const [data, setData] = useState({})
-    const [isLoading,setIsLoading] = useState(true);
-    const [error,setError] = useState(null);
+    // const [data, setData] = useState({})
+    // const [isLoading,setIsLoading] = useState(true);
+    // const [error,setError] = useState(null);
     
+    const [{data,isLoading,error},dispatch] = useReducer(reducer,initialState)
+
     const { id } = useParams();
 
     useEffect(() => {
-        // getData();
+        
         apiGet(`/shows/${id}`).then(result=>{
-            setData(result);
-            setIsLoading(false)
+            dispatch({
+                type:'FETCH_SUCCESS',
+                data: result
+            })
+            // setData(result);
+            // setIsLoading(false)
         }).catch(err=>{
-            setIsLoading(false);
-            setError(err.message)
+            dispatch({
+                type:'FETCH_FAILED',
+                error:err.message
+            })
+            // setIsLoading(false);
+            // setError(err.message)
 
         })
     }, [id])
@@ -27,7 +66,7 @@ const Show = () => {
     
 
     if(isLoading){
-        return "<div>Date is data loaded</div>"
+        return "Date is data loaded"
     }
 
     if(error){
